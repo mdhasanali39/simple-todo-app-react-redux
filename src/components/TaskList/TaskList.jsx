8d4d99f625ee706b02f583db9cd9fbbab7240d47
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-04-16 12:21:19 +0600

    TaskList updated with added static task

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,9 +1,37 @@
+import { Box, Grid, Paper, Typography, Checkbox } from "@mui/material";
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import { red } from "@mui/material/colors";
+
 const TaskList = () => {
-    return (
-        <div>
-            TaskList
-        </div>
-    );
+  const taskStyles = {
+    position:'relative',
+    display:'flex',
+    alignItems:'center',
+    borderRadius: "15px",
+    transition: "all .2s linear",
+    "&:hover": {
+      backgroundColor: "rgb(191 219 254)",
+    },
+  };
+
+  return (
+    <Box component="div" paddingBlock="30px" maxWidth="70%" margin="0 auto">
+        <Typography variant="h4" color="primary" marginBottom="10px">
+            All Tasks 
+        </Typography>
+      <Grid container spacing={2} >
+        <Grid item xs={12}>
+            <Paper sx={taskStyles}>
+              <Checkbox color="success" size="lg" />
+              <Typography sx={{ "font-size": "16px" }} component="p">
+                todo task
+              </Typography>
+              <RemoveCircleIcon sx={{position:'absolute', right:'5px',cursor:'pointer',color: red[600]}}/>
+            </Paper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default TaskList;
