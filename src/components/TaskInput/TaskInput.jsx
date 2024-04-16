bf4eda32b52a5ecd2c81aa75035ec397ab223989
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-04-16 12:20:20 +0600

    updated by added task input field and create task button

--- src/components/TaskInput/TaskInput.jsx
+++ src/components/TaskInput/TaskInput.jsx
@@ -1,9 +1,39 @@
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+} from "@mui/material";
+
 const TaskInput = () => {
-    return (
-        <div>
-            TaskInput
-        </div>
-    );
+
+  return (
+    <Box
+      component="div"
+      display="flex"
+      justifyContent="center"
+      p="16px"
+    >
+      <Box component="form">
+        <Box component="div" paddingBlock="40px" display="flex">
+          <FormControl>
+            <InputLabel htmlFor="my-input">Enter Your Task</InputLabel>
+            <Input id="my-input" type="text" />
+          </FormControl>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            type="submit"
+            sx={{ marginLeft: "5px", height: "100%", alignSelf: "flex-end" }}
+          >
+            Create
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default TaskInput;
